4445ef2ddb9ecb2a5e218ec44288b7836fc22f2a
n53f <n53337@users.noreply.github.com>
2023-01-04 17:22:23 +0000

    adding tailwindcss

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,7 @@ import "../../styles/App.css";
 
 function App() {
   return (
-    <h1>
+    <h1 className="">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque in maxime
       sit nam deleniti fuga quibusdam, modi, vero ad mollitia explicabo tempore
       nesciunt iure quae? Laboriosam porro vel iure vitae.
