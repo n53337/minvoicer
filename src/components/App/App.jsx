fedee441533464760c8a1707f96189ebd1fe9674
n53f <n53337@users.noreply.github.com>
2023-01-05 05:44:23 +0000

    router setup

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,37 @@
 import "../../styles/App.css";
+import { RouterProvider, Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <a href="">Hey</a>
-      <h1>AM H1</h1>
-      <p>AM P</p>
-      <button className="btn btn-primary">Am Button</button>
-      <button className="btn btn-primary-w">Am Button</button>
-      <button className="btn btn-accent">Am Button</button>
-      <button className="btn btn-accent-w">Am Button</button>
-    </>
+    <Routes>
+      <Route path="/">
+        <Route index element={<h1>Home</h1>} />
+
+        <Route path="register" element={<h1>register</h1>} />
+
+        <Route path="login" element={<h1>login</h1>} />
+
+        <Route path="dashboard" element={<h1>dashboard</h1>} />
+
+        <Route path="profile" element={<h1>profile</h1>} />
+
+        <Route path="invoices">
+          <Route index element={<h1>invoices</h1>} />
+          <Route path="new" element={<h1>new invoice</h1>} />
+        </Route>
+
+        <Route path="customers">
+          <Route index element={<h1>customers</h1>} />
+          <Route path="new" element={<h1>new customer</h1>} />
+        </Route>
+
+        <Route path="products">
+          <Route index element={<h1>products</h1>} />
+          <Route path="new" element={<h1>new product</h1>} />
+        </Route>
+      </Route>
+      <Route path="*" element={"404"}></Route>
+    </Routes>
   );
 }
 
