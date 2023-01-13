355bdbd50265508ac35aee29efc4b76d2761dfcb
n53f <n53337@users.noreply.github.com>
2023-01-13 08:48:18 +0000

    context initial

---
+++ src/Context/GlobalContext.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GlobalContext = () => {
+  return <div>GlobalContext</div>;
+};
+
+export default GlobalContext;
---
+++ src/Context/GlobalReducer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GlobalReducer = () => {
+  return <div>GlobalReducer</div>;
+};
+
+export default GlobalReducer;
